bedf3c94c496546cd397cb282738b9a84a93f4bf
MaxSliw <tallcraft@outlook.com>
2016-12-04 21:57:44 +0100

    Condition event video pour savoir si elle existe

--- javascript/script.js
+++ javascript/script.js
@@ -4,16 +4,18 @@ const video = document.getElementById('video');
 const textVideo = document.getElementById('titre_accueil');
 const nav = document.getElementById('nav_responsive');
 
-video.addEventListener("timeupdate", function () {
-    if (this.currentTime >= 9.5) {
-        video.pause();
-        textVideo.style.visibility = "visible";
-        textVideo.style.animationName = "fade";
-        textVideo.style.opacity = 1;
-        document.getElementsByTagName('span')[0].style.animationName = "boom";
-        document.getElementsByTagName('span')[1].style.animationName = "boom";
-    }
-});
+if (video != null) {
+    video.addEventListener("timeupdate", function () {
+        if (this.currentTime >= 9.5) {
+            video.pause();
+            textVideo.style.visibility = "visible";
+            textVideo.style.animationName = "fade";
+            textVideo.style.opacity = 1;
+            document.getElementsByTagName('span')[0].style.animationName = "boom";
+            document.getElementsByTagName('span')[1].style.animationName = "boom";
+        }
+    });
+}
 
 /*document.addEventListener("mouseup", function (e) {
     if (!nav.is(e.target) // if the target of the click isn't the container...
