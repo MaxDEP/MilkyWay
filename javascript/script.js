9db9ae4fdfc32523c08c31daea8fc82229b5ecd9
MaxSliw -TallCraft <MaxSliw -TallCraft>
2016-12-12 18:41:20 +0100

    Barre de recherche planètes galerie

--- javascript/script.js
+++ javascript/script.js
@@ -3,6 +3,8 @@
 const video = document.getElementById('video');
 const textVideo = document.getElementById('titre_accueil');
 const nav = document.getElementById('nav_responsive');
+const recherche = document.getElementById('recherche');
+const galerie = document.getElementById('galerie');
 
 if (video != null) {
     video.addEventListener("timeupdate", function () {
@@ -17,15 +19,6 @@ if (video != null) {
     });
 }
 
-/*document.addEventListener("mouseup", function (e) {
-    if (!nav.is(e.target) // if the target of the click isn't the container...
-        &&
-        nav.has(e.target).length === 0) // ... nor a descendant of the container
-    {
-        closeNav();
-    }
-});*/
-
 function openNav() {
     document.getElementById("nav_responsive").style.width = "250px";
 }
@@ -34,3 +27,20 @@ function closeNav() {
     document.getElementById("nav_responsive").style.width = "0";
 
 }
+
+function setFirstImg(alt){
+    
+}
+
+find = function () {
+    for(let i = 0; i < galerie.children.length; i++){
+        if(galerie.children[i].childNodes[0].alt == recherche.value && galerie.children[i].className != "first"){
+            galerie.children[i].className += "first";
+        }else if(galerie.children[i].className == "first"){
+            galerie.children[i].className = "";
+        }
+    }
+};
+
+recherche.addEventListener('keydown', find, false);
+recherche.addEventListener('keyup', find, false);
